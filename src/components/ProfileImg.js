0567c94dee798a9287e46ab5d9ccdd4201a7d83a
dgh500 <dgh500@gmail.com>
2020-02-06 19:02:18 +0000

    heroku steps

---
+++ src/components/ProfileImg.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Renders the user's profile image from their username
+ * @public
+ * @param {string} username - The user to render the profile image of
+ * @param {int} width - Image width ( defaults to 100 ) in pixels
+ * @param {int} height - Image width ( defaults to match height )
+ */
+const ProfileImg = props => {
+  // todo
+  return (
+
+  );
+}
+
+ProfileImg.propTypes = {
+  /** The user to load an image for */
+  username: PropTypes.string
+}
+
+
+export default ProfileImg;
